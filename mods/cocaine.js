f2c53758eead5a367787e8ca6836a025e491182b
yourmom64209 <2jes1366@gmail.com>
2025-03-08 02:01:53 -0800

    Update cocaine.js

--- mods/cocaine.js
+++ mods/cocaine.js
@@ -1,17 +1,18 @@
+elements.liquid_cocaine = {
+	color:"#FFFFFF",
+	behavior: behaviors.LIQUIDS,
+	category: "drugs",
+	state: "liquid",
+	stateLow:"cocaine",
+	tempLow: 0,
+}
+
 elements.cocaine = {
 	color:"#FFFFFF",
-	behavior: behaviors.powder,
+	behavior: behaviors.POWDERS,
 	category: "drugs",
 	state: "powder",
 	stateHigh:"liquid_cocaine",
 	tempHigh: 100,
 }
 
-elements.liquid_cocaine = {
-	color:"#FFFFFF",
-	behavior: behaviors.liquid,
-	category: "drugs",
-	state: "liquid",
-	stateLow:"cocaine",
-	tempLow: 0,
-}
