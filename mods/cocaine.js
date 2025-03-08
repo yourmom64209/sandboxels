5470334e13bddd8d1641d2096724afb862a74846
yourmom64209 <2jes1366@gmail.com>
2025-03-08 02:20:56 -0800

    Add files via upload

--- mods/cocaine.js
+++ mods/cocaine.js
@@ -1,12 +1,3 @@
-elements.liquid_cocaine = {
-	color:"#FFFFFF",
-	behavior: behaviors.LIQUID,
-	category: "drugs",
-	state: "liquid",
-	stateLow:"cocaine",
-	tempLow: 0,
-}
-
 elements.cocaine = {
 	color:"#FFFFFF",
 	behavior: behaviors.POWDER,
